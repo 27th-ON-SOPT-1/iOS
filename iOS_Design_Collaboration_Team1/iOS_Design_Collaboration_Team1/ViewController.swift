b9ffc99b529a61dce9f7b19f2ed536d3222d6ac3
hansolnoh95 <hansolnoh95@naver.com>
2020-11-14 13:17:36 +0900

    feat: add project directory

---
+++ iOS_Design_Collaboration_Team1/iOS_Design_Collaboration_Team1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Design_Collaboration_Team1
+//
+//  Created by 노한솔 on 2020/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
